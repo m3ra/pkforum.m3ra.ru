584077482c8265579c1638c9d970015dc279523e
Petr Shevtsov <petr.shevtsov@gmail.com>
2019-07-24 23:23:58 +0300

    Initial commit

---
+++ functions/README.md
@@ -0,0 +1,17 @@
+# pkforum.m3ra.ru
+
+## Configuration
+
+```
+firebase functions:config:set \
+    mailgun.apikey="MAILGUN_API_KEY" \
+    mailgun.domain="YOUR_DOMAIN_NAME" \
+    mailgun.from="Your Name <your@email.com>" \
+    subject.confirm="Subject for Confirmation Message" \
+    subject.success="subject for Success Message" \
+    template.confirm="template.confirm" \
+    template.success="template.success" \
+    url.confirm="https://example.com/confirm" \
+    url.success="https://example.com/success" \
+    hash.salt="some random string"
+```
---
+++ functions/index.js
@@ -0,0 +1,41 @@
+const crypto = require('crypto');
+const functions = require('firebase-functions');
+const express = require('express');
+const { check, validationResult } = require('express-validator');
+
+const app = express();
+
+app.post('/signup', [check('email').isEmail()], (req, res) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.array() });
+  }
+
+  const email = req.body.email;
+  const mailgun = require('mailgun-js')({
+    apiKey: functions.config().mailgun.apikey,
+    domain: functions.config().mailgun.domain
+  });
+
+  const hash = crypto.createHash('sha256');
+  hash.update(email + functions.config().hash.salt);
+
+  const data = {
+    from: functions.config().from,
+    to: email,
+    subject: functions.config().subject.confirm,
+    template: functions.config().template.confirm,
+    'v:email': email,
+    'v:hash': hash.digest('hex')
+  };
+
+  mailgun.messages().send(data, (err, body) => {
+    console.log(err, body);
+    return res.redirect(functions.config().url.confirm);
+  });
+
+  return null;
+});
+
+exports.app = functions.https.onRequest(app);
